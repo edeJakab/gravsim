eb3998e17919a527385cccc95b6609428bf23280
JAKAB EL. EDE KRISZTIÁN <jakab.ede@infolab.bolyai.ro>
2026-01-14 10:51:21 +0200

    commit

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 # Example file showing a circle moving on screen
 import pygame
 
-# pygame setup
+class body:
+    def __init__(self, mass, pos, vel, acc):
+        self.mass = mass
+        self.pos = pygame.Vector2(pos)
+        self.vel = pygame.Vector2(vel)
+        self.acc = pygame.Vector2(acc)
+    def update_state(self, dt):
+        self.pos += self.vel * dt
+        self.vel += self.acc * dt
+
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 dt = 0
+dtxd = 0.01
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+init_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+ball = body(10, init_pos, pygame.Vector2(10, 10), pygame.Vector2(100, 0))
 
 while running:
     # poll for events
@@ -20,17 +32,17 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
+    pygame.draw.circle(screen, "red", ball.pos, 40)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
+        ball.pos.y -= 300 * dt
     if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
+        ball.pos.y += 300 * dt
     if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
+        ball.pos.x -= 300 * dt
     if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+        ball.pos.x += 300 * dt
 
     # flip() the display to put your work on screen
     pygame.display.flip()
@@ -39,5 +51,6 @@ while running:
     # dt is delta time in seconds since last frame, used for framerate-
     # independent physics.
     dt = clock.tick(60) / 1000
+    ball.update_state(dtxd)
 
 pygame.quit()
