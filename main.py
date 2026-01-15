efc43b2b9ee963e05202501e738aacbbd9e5ef2d
JAKAB EL. EDE KRISZTIÁN <jakab.ede@infolab.bolyai.ro>
2026-01-15 10:50:57 +0200

    info 1

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import pygame
 
+class camera:
+    def __init__(self, zoom, pos):
+        self.zoom = zoom # compared to in-game. zoom = 1 means 1:1 correspondence
+        self.pos = pygame.Vector2(pos) # position of the top left corner of the camera in in-game coordinates
+    def world_to_screen(self, world_pos):
+        return world_pos * camera.zoom + camera.pos
+    def scale(self, rad):
+        return rad * camera.zoom
+
 class body:
-    def __init__(self, mass, pos, vel, acc):
+    def __init__(self, mass, rad, pos, vel, acc):
         self.mass = mass
+        self.rad = rad
         self.pos = pygame.Vector2(pos)
         self.vel = pygame.Vector2(vel)
         self.acc = pygame.Vector2(acc)
@@ -10,10 +20,11 @@ class body:
         self.pos += self.vel * dt
         self.vel += self.acc * dt
 
-class camera:
-    def __init__(self, zoom, pos):
-        self.zoom = zoom # compared to in-game. zoom = 1 means 1:1 correspondence
-        self.pos = pygame.Vector2(pos) # position of the top left corner of the camera in in-game coordinates
+def wall_collision(body, x_min, x_max, y_min, y_max):
+    if body.pos.x - body.rad <= x_min or body.pos.x + body.rad >= x_max:
+        body.vel.x = -body.vel.x
+    if body.pos.y - body.rad <= y_min or body.pos.y + body.rad >= y_max:
+        body.vel.y = -body.vel.y
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
@@ -22,11 +33,10 @@ running = True
 dt = 0
 dtxd = 0.01
 
-camera = camera(5, pygame.Vector2(0, 0))
-
+camera = camera(1, pygame.Vector2(0, 0))
 
 init_pos = pygame.Vector2(screen.get_width() / 9, screen.get_height() / 9)
-ball = body(10, init_pos, pygame.Vector2(10, 10), pygame.Vector2(0, 0))
+ball = body(10, 40, init_pos, pygame.Vector2(2000, 1000), pygame.Vector2(0, 0))
 
 while running:
     # poll for events
@@ -35,10 +45,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
-
-    pygame.draw.circle(screen, "red", ball.pos * camera.zoom + camera.pos, 40 * camera.zoom)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
@@ -51,13 +58,12 @@ while running:
         ball.pos.x += 300 * dt
 
     # flip() the display to put your work on screen
+    wall_collision(ball, 0, 1280, 0, 720)
+    ball.update_state(dtxd)
+    print (ball.pos, flush=True)
+
+    pygame.draw.circle(screen, "red", camera.world_to_screen(ball.pos), camera.scale(ball.rad))
     pygame.display.flip()
 
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
     dt = clock.tick(60) / 1000
-    ball.update_state(dtxd)
-
-    print (screen.get_width(), flush=True)
 pygame.quit()
