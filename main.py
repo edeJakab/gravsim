79aadc88ddefc247b0401ba153e59ac6b394e2dd
JAKAB EL. EDE KRISZTIÁN <jakab.ede@infolab.bolyai.ro>
2026-01-29 10:30:26 +0200

    split into two files

--- main.py
+++ main.py
@@ -1,45 +1,5 @@
 import pygame
-
-grav_constant = 6.674e-11
-
-class Camera:
-    def __init__(self, zoom, pos):
-        self.zoom = zoom # compared to in-game. zoom = 1 means 1:1 correspondence
-        self.pos = pygame.Vector2(pos) # position of the top left corner of the camera in in-game coordinates
-    def world_to_screen(self, world_pos):
-        return world_pos * self.zoom + self.pos
-    def scale(self, rad):
-        return rad * self.zoom
-
-class Body:
-    def __init__(self, mass, rad, pos, vel, acc):
-        self.mass = mass
-        self.rad = rad
-        self.pos = pygame.Vector2(pos)
-        self.vel = pygame.Vector2(vel)
-        self.acc = pygame.Vector2(acc)
-    def update_state(self, dt):
-        self.pos += self.vel * dt + self.acc * dt * dt / 2
-        self.vel += self.acc * dt
-
-def wall_collision(body, x_min, x_max, y_min, y_max):
-    if body.pos.x - body.rad <= x_min:
-        body.pos.x = x_min + body.rad
-        body.vel.x = -body.vel.x
-    if body.pos.x + body.rad >= x_max:
-        body.pos.x = x_max - body.rad
-        body.vel.x = -body.vel.x
-    if body.pos.y - body.rad <= y_min:
-        body.pos.y = y_min + body.rad
-        body.vel.y = -body.vel.y
-    if body.pos.y + body.rad >= y_max:
-        body.pos.y = y_max - body.rad
-        body.vel.y = -body.vel.y
-
-def grav_update(body1, body2):
-    vec12 = body2.pos - body1.pos
-    body1.acc = (grav_constant * body2.mass / (pygame.Vector2.magnitude(vec12) ** 2) ) * pygame.Vector2.normalize(vec12)
-    body2.acc = (grav_constant * body1.mass / (pygame.Vector2.magnitude(vec12) ** 2) ) * pygame.Vector2.normalize(-vec12)
+from engine import *
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
@@ -49,6 +9,8 @@ dt = 0
 dtxd = 0.55
 
 cam = Camera(1, pygame.Vector2(100, 0))
+
+bodies = []
 
 xi = 600
 yi = 300
@@ -82,7 +44,7 @@ while running:
     if keys[pygame.K_e]:
         cam.zoom *= 0.99
     
-    grav_update(ball, ball2)
+    grav_update_pair(ball, ball2)
     ball.update_state(dtxd)
     ball2.update_state(dtxd)
     wall_collision(ball, 0, 1280, 0, 720)
@@ -91,8 +53,6 @@ while running:
     pygame.draw.circle(screen, "red", cam.world_to_screen(ball.pos), cam.scale(ball.rad))
     pygame.draw.circle(screen, "red", cam.world_to_screen(ball2.pos), cam.scale(ball2.rad))
     pygame.display.flip()
-    
-    print(ball2.vel, flush=True)
 
     dt = clock.tick(60) / 1000
 pygame.quit()
